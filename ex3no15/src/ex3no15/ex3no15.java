5c725d9b8ffc92c17fae4413ecf8a57cd4c90f68
admin <admin@DESKTOP-8L2IG92>
2021-04-22 06:38:34 +0900

    ex3no15

---
+++ ex3no15/src/ex3no15/ex3no15.java
@@ -0,0 +1,10 @@
+package ex3no15;
+
+public class ex3no15 {  //105p
+	public static void main(String[] args) {
+		char lowerCase = 'a';
+		char upperCase = (char)(lowerCase - 32);
+		System.out.println(upperCase);
+	}
+
+}
